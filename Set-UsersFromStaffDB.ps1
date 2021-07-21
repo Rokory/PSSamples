46e47acf70c3ced9c4a3fcb5bfc0e05b72627f8d
Roman Korecky <Roman.Korecky@easyon.at>
2021-07-21 09:48:51 +0200

    Streamlined scripts

--- Set-UsersFromStaffDB.ps1
+++ Set-UsersFromStaffDB.ps1
@@ -7,16 +7,23 @@ $datareader = Get-SqlDataReader -Query 'SELECT * FROM Personen' -Connection $con
 
 # While there are new records
 while ($datareader.Read()) {
-    # Get some field called name
-    $name = $datareader['Name']
+    <#
+    Der Operator -f setzt die Werte, die nach ihm in einer
+    Komma-getrennten Liste folgen in die Platzhalter der Zeichenkette
+    vor dem Operator ein. Die Platzhalter werden als {0}, {1}, {2} usw.
+    geschrieben. Der erste Wert wird bei {0} eingesetzt, der zweite bei
+    {1} usw. Die Werte können auch formatiert werden. Siehe auch:
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_operators?view=powershell-7.1#special-operators
+    #>
+    $filter = 'Name -eq "{0}"' -f $datareader['Name']
 
     # Find user with this name in AD
     # and set the extensionAttribute 9 to the value of the extension field
-    Get-ADUser -Filter ('Name -eq "{0}"' -f  $name) |
+    Get-ADUser -Filter $filter |
     Set-ADUser -Replace @{ extensionAttribute9 = $datareader['Extension'] }
 
     # Check, if we were successful
-    Get-ADUser -Filter ('Name -eq "{0}"' -f  $name) -Properties 'ExtensionAttribute9' | Select-Object Name, ExtensionAttribute9
+    Get-ADUser -Filter $filter -Properties 'ExtensionAttribute9' | Select-Object Name, ExtensionAttribute9
 }
 
 # Clean up
--- Setup-StaffDB.ps1
+++ Setup-StaffDB.ps1
@@ -5,13 +5,13 @@ $databaseName = 'Personal'
 New-SqlDatabase -Name $databaseName -Server $server
 
 # Define command to create the table
-$command = @"
+$command = @'
 CREATE TABLE [dbo].[Personen](
 	[ID] [int] IDENTITY(1,1) PRIMARY KEY,
 	[Name] [nvarchar](100) NOT NULL,
 	[Extension] [nvarchar](100) NULL
 )
-"@
+'@
 
 # Run command
 $connection = Connect-SqlServer -Database 'Personal' -Server $server
